f3d5e14ba4d3c84fbd212dd4f79f579d44bc334f
zhixin <wenzhixin2010@gmail.com>
2015-07-07 07:49:16 +0800

    Update Tasks.find

--- server/publications.js
+++ server/publications.js
@@ -1,3 +1,14 @@
 Meteor.publish('tasks', function () {
-    return Tasks.find({});
+    var date = new Date();
+
+    date.setHours(0);
+    date.setMinutes(0);
+    date.setSeconds(0);
+
+    return Tasks.find({
+        $or: [{
+            createdAt: {$gte: date}
+        }, {
+            checked: {$ne: true}
+        }]});
 });
